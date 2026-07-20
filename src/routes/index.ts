92c544cebfe0faa60035d33733909dca611bd47a
Subham-112 <subhambusiness286@gmail.com>
2026-07-20 16:32:29 +0530

    added that seller routes in global use

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRoutes from "../modules/user/user.routes";
+import sellerRoutes from "../modules/seller/seller.router";
 
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/sellers", sellerRoutes);
 
 export default router;
