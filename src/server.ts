3811cfef267f16de6155d6a5641de2a7ed93b112
Subham-112 <subhambusiness286@gmail.com>
2026-08-06 20:40:49 +0530

    refactor: update database connection import to use named export for consistency

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { config as env } from "./config/config";
 import { app } from "./app";
 import { config } from "dotenv";
 import { watchEnvFile } from "./config/envWatcher";
-import connectDB from "./config/database";
+import { connectDB } from "./config/database";
 import { initializeJobs } from "./jobs";
 import { logger } from "./config/logger";
 
